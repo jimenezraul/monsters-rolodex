2314192758f48b9c004c7e23dc9ec6c8c70e562b
jimenezraul <jimenezraul1981@gmail.com>
2020-08-31 20:16:37 -0400

    Update

---
+++ precache-manifest.5bef9d0e8fcd4b3fbc69aa296833a371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08f18277dc749ecd6f8ae83db8c97de",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0d4f9a577798595de4e9",
+    "url": "/monsters-rolodex/static/css/main.9b0e1429.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4f9a577798595de4e9",
+    "url": "/monsters-rolodex/static/js/main.5b0c0c23.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
